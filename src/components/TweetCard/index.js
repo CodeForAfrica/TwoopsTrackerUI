4e2347c7f8a16bb52e635b9b67f9104f7a172ada
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-10-18 23:35:47 +0200

    base files

---
+++ src/components/TweetCard/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const TweetCard = () => <div>Tweet Component</div>;
+
+export default TweetCard;
---
+++ src/components/TweetCard/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TweetCard from ".";
+
+export default {
+  title: "Components/TweetCard",
+  argTypes: {},
+};
+
+const Template = () => <TweetCard />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
