5ebb3bc15af7889c3f3ecc8568d0597616d850cf
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-10-24 11:44:37 +0200

    fix storybook error

--- src/components/Tweets/index.stories.js
+++ src/components/Tweets/index.stories.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import Tweets from ".";
 
+import config from "@/twoopstracker/config";
+
 export default {
   title: "Components/Tweets",
   argTypes: {},
@@ -12,15 +14,5 @@ const Template = (args) => <Tweets {...args} />;
 export const Default = Template.bind({});
 
 Default.args = {
-  retweet:
-    "<span class='highlight'>Retweet @handle</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent faucibus est ac #vestibulum eleifend. Suspendisse eget aliquam lorem nu...",
-  originalTweet:
-    "Original tweet by  <span class='highlight'>Link to original tweet</span>",
-  username: "Dave Chappelle",
-  handle: "<span class='highlight'>@handle</span> Type of account",
-  listDescription: "Add to List",
-  posted: "Posted on mm/dd/yyyy at 00:00:00",
-  deleted:
-    "Deleted after xx minutes <span class='highlight'>about x hours ago</span>",
-  interactions: "Number of interactions: xxxx",
+  tweets: config.tweets,
 };
