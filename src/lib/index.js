86fd65fe2a374e41863633a1009d5a9e5d8d9bc5
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-11-08 09:04:42 +0300

    move search logic

--- src/lib/index.js
+++ src/lib/index.js
@@ -7,6 +7,27 @@ export const fetchTweets = async () => {
   return data;
 };
 
-export const fetchLists = () => {
-  console.log("Fetch Lists", url);
+export const fetchSearchTweets = async (search, date, theme, location) => {
+  let searchUrl = `${url}/tweets/?location=${location}`;
+
+  if (search && theme) {
+    searchUrl = `${searchUrl}&&query="${theme} and ${search}"`;
+  } else {
+    searchUrl = `${searchUrl}&&query=${theme || search}`;
+  }
+
+  if (date) {
+    const newDate = new Date();
+    const today = newDate.toISOString().substr(0, 10);
+
+    newDate.setDate(newDate.getDate() - date);
+    const pastDate = newDate.toISOString().substr(0, 10);
+
+    searchUrl = `${searchUrl}&&startDate=${pastDate}&&endDate=${today}`;
+  }
+
+  const res = await fetch(searchUrl);
+  const data = await res.json();
+
+  return data;
 };
