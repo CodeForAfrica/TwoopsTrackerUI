6e68334963786092c8028622f26c938031d7cd0a
Obed Amoasi <obedamoasi@gmail.com>
2021-11-09 15:53:13 +0000

    [wip] update homepage components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
-import { Typography } from "@material-ui/core";
 import React from "react";
 
+import Banner from "@/twoopstracker/components/Banner";
+import CallToAction from "@/twoopstracker/components/CallToAction";
+import Investigations from "@/twoopstracker/components/Investigations";
 import Page from "@/twoopstracker/components/Page";
+import Partners from "@/twoopstracker/components/Partners";
 
 export default function Index() {
   return (
-    <>
-      <Typography>One Two Three</Typography>
-      <Page />
-    </>
+    <Page>
+      <Banner />
+      <CallToAction />
+      <Investigations />
+      <Partners />
+    </Page>
   );
 }
