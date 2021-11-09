d97fda6967e617e280b3604f2c4f8e4c8635e3d4
_ Kilemensi <clemence@codeforafrica.org>
2021-11-09 15:49:27 +0300

    Switch to server-side search + config for SWR

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,44 @@
+import PropTypes from "prop-types";
 import React from "react";
+import { SWRConfig } from "swr";
 
 import Page from "@/twoopstracker/components/Page";
 import TweetsContainer from "@/twoopstracker/components/TweetsContainer";
-import { fetchTweets } from "@/twoopstracker/lib";
+import { search } from "@/twoopstracker/lib";
 
-export default function Index(props) {
+export default function Index({ fallback, tweets, ...props }) {
   return (
     <>
-      <TweetsContainer {...props} />
-      <Page />
+      <Page {...props}>
+        <SWRConfig value={{ fallback }}>
+          <TweetsContainer tweets={tweets} />
+        </SWRConfig>
+      </Page>
     </>
   );
 }
 
-export async function getServerSideProps() {
-  const data = await fetchTweets();
+Index.propTypes = {
+  fallback: PropTypes.arrayOf(PropTypes.shape({})),
+  tweets: PropTypes.arrayOf(PropTypes.shape({})),
+};
 
-  // Pass data to the page via props
-  return { props: { data } };
+Index.defaultProps = {
+  fallback: undefined,
+  tweets: undefined,
+};
+
+// TODO(kilemensi): Once search has been moved to the search page, this method
+//                  should be turned into getStaticProps
+export async function getServerSideProps() {
+  const tweets = await search();
+
+  return {
+    props: {
+      tweets,
+      fallback: {
+        "/api/search": tweets,
+      },
+    },
+  };
 }
