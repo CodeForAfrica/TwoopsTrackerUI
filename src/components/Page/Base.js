0a4634e41d559644b2bb83eb5295d5bf9854bff5
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2021-11-11 12:03:55 +0300

    Add navigation to page

--- src/components/Page/Base.js
+++ src/components/Page/Base.js
@@ -2,12 +2,16 @@ import { NextSeo } from "next-seo";
 import PropTypes from "prop-types";
 import React from "react";
 
+import Navigation from "@/twoopstracker/components/Navigation";
+import { navigationArgs } from "@/twoopstracker/config";
+
 /**
  * Base page that can be used to build all other pages.
  */
 function BasePage({ children, ...props }) {
   return (
     <div>
+      <Navigation {...navigationArgs} />
       <NextSeo {...props} />
       {children}
     </div>
