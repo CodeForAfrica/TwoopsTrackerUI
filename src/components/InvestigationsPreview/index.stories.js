cd7902e9a5be9b5e2b27b31b8e241e777d3d619f
Obed Amoasi <obedamoasi@gmail.com>
2021-11-11 06:51:49 +0000

    refactor investigations preview

---
+++ src/components/InvestigationsPreview/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import InvestigationsPreview from ".";
+
+export default {
+  title: "Components/InvestigationsPreview",
+  argTypes: {},
+};
+
+const Template = () => <InvestigationsPreview />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
