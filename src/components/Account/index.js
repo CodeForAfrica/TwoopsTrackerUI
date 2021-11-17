98d8a1a80f0e99e440343109a597d91659d9a6db
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-11-17 07:48:11 +0300

    flesh out component

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -1,10 +1,40 @@
+import { Grid, Typography, Button } from "@material-ui/core";
+import Image from "next/image";
 import React from "react";
 
 import useStyles from "./useStyles";
 
+import UserIcon from "@/twoopstracker/assets/icons/user.svg";
+
 const Account = (props) => {
   const classes = useStyles(props);
-  return <div className={classes.root}>Account</div>;
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item lg={1}>
+          <div className={classes.icon}>
+            <Image layout="fill" src={UserIcon} />
+          </div>
+        </Grid>
+        <Grid item lg={5} sm={12} className={classes.detailSection}>
+          <Typography className={classes.username}>
+            Username Username
+          </Typography>
+          <Typography className={classes.handle}>@handle</Typography>
+          <Typography className={classes.accountType}>
+            Type of account
+          </Typography>
+          <Typography className={classes.list}>
+            Saved on mm/dd/yy at 00:00:00
+          </Typography>
+        </Grid>
+        <Grid item lg={5} sm={12} className={classes.buttonSection}>
+          <Button className={classes.edit}>Edit</Button>
+          <Button className={classes.delete}>Delete</Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Account;
--- src/components/Account/useStyles.js
+++ src/components/Account/useStyles.js
@@ -1,7 +1,50 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(({ typography, breakpoints }) => ({
   root: {},
+  icon: {
+    position: "relative",
+    width: typography.pxToRem(100),
+    height: typography.pxToRem(100),
+    border: `solid 1px`,
+    "& img": {
+      padding: `${typography.pxToRem(10)} !important`,
+    },
+  },
+  detailSection: {
+    marginLeft: 0,
+    [breakpoints.up("md")]: {
+      marginLeft: typography.pxToRem(20),
+    },
+  },
+  username: {
+    fontWeight: "700",
+    fontSize: typography.pxToRem(36),
+  },
+  handle: {
+    display: "inline",
+    color: "#DB1111",
+  },
+  accountType: {
+    display: "inline",
+    marginLeft: typography.pxToRem(10),
+  },
+  list: {
+    marginTop: typography.pxToRem(10),
+  },
+  buttonSection: {
+    display: "flex",
+    justifyContent: "end",
+    alignItems: "end",
+  },
+  edit: {
+    color: "#DB1111",
+  },
+  delete: {
+    backgroundColor: "#DB1111",
+    color: "#fff",
+    marginLeft: typography.pxToRem(10),
+  },
 }));
 
 export default useStyles;
