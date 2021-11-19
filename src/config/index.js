44e24e84b6e05b245172e6d71f2d145e359ad0f9
_ Kilemensi <clemence@codeforafrica.org>
2021-11-19 07:58:41 +0300

    Add pagination config

--- src/config/index.js
+++ src/config/index.js
@@ -865,6 +865,12 @@ export const resources = {
   ],
 };
 
+export const pagination = {
+  pageSizeDefaultValue: "20",
+  pageSizeLabel: "Results on Page",
+  pageSizeOptions: [{ value: "20" }, { value: "50" }],
+};
+
 const config = {
   tweets,
   navigationArgs,
