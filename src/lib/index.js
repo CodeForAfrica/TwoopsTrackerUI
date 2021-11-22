16706335b4a67570d4174f1b8391d99271a0d83c
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-11-22 23:27:15 +0300

    fix conflicts

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,11 +2,6 @@ import { subDays } from "date-fns";
 
 const BASE_URL = process.env.TWOOPSTRACKER_API_URL;
 
-export async function tweets() {
-  const res = await fetch(`${BASE_URL}/tweets/`);
-  return res.json();
-}
-
 export async function lists() {
   const res = await fetch(`${BASE_URL}/lists/`);
   return res.json();
@@ -77,12 +72,19 @@ export async function APIRequest(payload, method, param) {
   return res.json();
 }
 
-export async function search({ term, theme, location, days = 7 }) {
+export async function fetchJson(url) {
+  const res = await fetch(url);
+  return res.json();
+}
+
+function tweetsSearchParamFromSearchQuery({
+  query,
+  location,
+  days = 7,
+  page,
+  pageSize,
+}) {
   const searchParams = new URLSearchParams();
-  let query = term || theme;
-  if (query && theme) {
-    query = `(${query} AND ${theme})`;
-  }
   if (query) {
     searchParams.append("query", query);
   }
@@ -92,10 +94,50 @@ export async function search({ term, theme, location, days = 7 }) {
   const date = new Date();
   const endDate = date.toISOString().substr(0, 10);
   const startDate = subDays(date, days).toISOString().substr(0, 10);
-  searchParams.append("startDate", startDate);
-  searchParams.append("endDate", endDate);
+  searchParams.append("start_date", startDate);
+  searchParams.append("end_date", endDate);
+  if (page) {
+    searchParams.append("page", page);
+  }
+  if (pageSize) {
+    searchParams.append("page_size", pageSize);
+  }
+  searchParams.append("format", "json");
 
-  const searchUrl = `${BASE_URL}/tweets/?${searchParams.toString()}&format=json`;
-  const res = await fetch(searchUrl);
-  return res.json();
+  return searchParams;
+}
+
+function tweetsSearchQueryFromUserQuery(userQuery) {
+  const {
+    query: term,
+    theme,
+    location,
+    days: daysAsString,
+    page,
+    pageSize,
+  } = userQuery;
+  let query = term || theme;
+  if (query && theme) {
+    query = `(${query} AND ${theme})`;
+  }
+  let days = parseInt(daysAsString, 10) || undefined;
+  if (days > 30) {
+    days = 30;
+  }
+  return { query, location, days, page, pageSize };
+}
+
+export async function tweets(userQuery = {}) {
+  const searchQuery = tweetsSearchQueryFromUserQuery(userQuery);
+  const searchParams = tweetsSearchParamFromSearchQuery(searchQuery);
+  const url = `${BASE_URL}/tweets/?${searchParams.toString()}`;
+  return fetchJson(url);
+}
+
+// Do not include page or pageSize in searchQuery
+export async function tweetsInsights({ page, pageSize, ...userQuery } = {}) {
+  const searchQuery = tweetsSearchQueryFromUserQuery(userQuery);
+  const searchParams = tweetsSearchParamFromSearchQuery(searchQuery);
+  const url = `${BASE_URL}/tweets/insights?${searchParams.toString()}`;
+  return fetchJson(url);
 }
