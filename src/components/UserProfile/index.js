e1a7938c1880bdbbc3afe6c2ee97911f91f64585
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2021-11-23 09:43:44 +0300

    style out the page

--- src/components/UserProfile/index.js
+++ src/components/UserProfile/index.js
@@ -4,39 +4,81 @@ import {
   ListItemText,
   Typography,
   Divider,
+  Grid,
+  Button,
 } from "@material-ui/core";
+import Image from "next/image";
 import React from "react";
 
+import useStyles from "./useStyles";
+
+import UserIcon from "@/twoopstracker/assets/icons/user.svg";
+import Link from "@/twoopstracker/components/Link";
 import Section from "@/twoopstracker/components/Section";
 
 function UserProfile() {
+  const classes = useStyles();
   return (
     <Section>
-      <List>
-        <ListItem>
-          <ListItemText>
-            <Typography>Lists</Typography>
-          </ListItemText>
-        </ListItem>
-        <Divider component="li" />
-        <ListItem>
-          <ListItemText>
-            <Typography>Saved Searches</Typography>
-          </ListItemText>
-        </ListItem>
-        <Divider component="li" />
-        <ListItem>
-          <ListItemText>
-            <Typography>Upload data</Typography>
-          </ListItemText>
-        </ListItem>
-        <Divider component="li" />
-        <ListItem>
-          <ListItemText>
-            <Typography>Your account</Typography>
-          </ListItemText>
-        </ListItem>
-      </List>
+      <Grid container>
+        <Grid item lg={3}>
+          <List>
+            <Link href="/accounts/lists" className={classes.link}>
+              <ListItem className={classes.listItem}>
+                <ListItemText>
+                  <Typography>Lists</Typography>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            <Link href="/accounts/search" className={classes.link}>
+              <ListItem className={classes.listItem}>
+                <ListItemText>
+                  <Typography>Saved Searches</Typography>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            <Link href="/accounts/upload" className={classes.link}>
+              <ListItem className={classes.listItem}>
+                <ListItemText>
+                  <Typography>Upload data</Typography>
+                </ListItemText>
+              </ListItem>
+            </Link>
+
+            <Link href="/accounts" className={classes.link}>
+              <ListItem className={classes.listItem}>
+                <ListItemText>
+                  <Typography>Your account</Typography>
+                </ListItemText>
+              </ListItem>
+            </Link>
+          </List>
+        </Grid>
+        <Grid item lg={9}>
+          <div className={classes.card}>
+            <Grid container>
+              <Grid item lg={1}>
+                <div className={classes.icon}>
+                  <Image layout="fill" src={UserIcon} />
+                </div>
+              </Grid>
+              <Grid item lg={10} className={classes.userDetails}>
+                <Typography className={classes.username}>
+                  Trolltracker Username
+                </Typography>
+                <Typography>City, Country</Typography>
+              </Grid>
+            </Grid>
+            <div className={classes.buttons}>
+              <Button className={classes.button}>Edit</Button>
+              <Button className={classes.button}>Delete</Button>
+            </div>
+            <Divider />
+          </div>
+        </Grid>
+      </Grid>
     </Section>
   );
 }
--- src/components/UserProfile/useStyles.js
+++ src/components/UserProfile/useStyles.js
@@ -1,7 +1,55 @@
 import { makeStyles } from "@material-ui/core/styles";
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles(({ typography }) => ({
   root: {},
+  card: {
+    marginTop: typography.pxToRem(20),
+    padding: `${typography.pxToRem(20)}`,
+    "& .highlight": {
+      color: "#DB1111",
+    },
+  },
+  icon: {
+    position: "relative",
+    width: typography.pxToRem(100),
+    height: typography.pxToRem(100),
+    border: `solid 1px`,
+    "& img": {
+      padding: `${typography.pxToRem(10)} !important`,
+    },
+  },
+  username: {
+    fontWeight: "700",
+    fontSize: typography.pxToRem(36),
+  },
+  userDetails: {
+    marginLeft: typography.pxToRem(30),
+  },
+  buttons: {
+    margin: `${typography.pxToRem(10)} 0`,
+  },
+  listItem: {
+    border: "solid 1px rgba(0,0,0,0.12)",
+    marginBottom: typography.pxToRem(1),
+    "&:hover": {
+      backgroundColor: "#f5f5f5",
+    },
+  },
+  link: {
+    color: "#000",
+    "&:hover": {
+      textDecoration: "none",
+    },
+  },
+  button: {
+    marginLeft: typography.pxToRem(5),
+    borderRadius: typography.pxToRem(25),
+    minWidth: typography.pxToRem(100),
+    border: "solid 1px transparent",
+    "&:hover": {
+      border: "solid 1px red",
+    },
+  },
 }));
 
 export default useStyles;
