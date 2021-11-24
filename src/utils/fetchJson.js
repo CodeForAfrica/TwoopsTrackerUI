841c60ffaa6e755046c742145bf785c3d04a3836
_ Kilemensi <kilemensi@users.noreply.github.com>
2021-11-24 09:25:24 +0300

    Remove extra }

--- src/utils/fetchJson.js
+++ src/utils/fetchJson.js
@@ -1,12 +1,11 @@
 async function fetchJson(url, session, options) {
   let fetchOptions = options;
   if (session?.accessToken) {
-      fetchOptions = { ...options };
-      fetchOptions.headers = {
-        ...fetchOptions.headers,
-        Authorization: `Bearer ${session.accessToken}`,
-      };
-    }
+    fetchOptions = { ...options };
+    fetchOptions.headers = {
+      ...fetchOptions.headers,
+      Authorization: `Bearer ${session.accessToken}`,
+    };
   }
 
   const res = await fetch(url, fetchOptions);
