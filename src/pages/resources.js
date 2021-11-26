c1575cb80d972171d19cb66f6466ad5d564a44e2
_ Kilemensi <clemence@codeforafrica.org>
2021-11-26 09:59:03 +0300

    Add title to pages/resources

--- src/pages/resources.js
+++ src/pages/resources.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import React from "react";
 
 import AboutContent from "@/twoopstracker/components/AboutContent";
@@ -5,11 +6,31 @@ import Hero from "@/twoopstracker/components/Hero";
 import Page from "@/twoopstracker/components/Page";
 import { resources } from "@/twoopstracker/config";
 
-export default function Resources({ ...props }) {
+function Resources({ resources: resourcesProp, ...props }) {
   return (
     <Page {...props}>
-      <Hero withCTA={false} {...resources.banner} />
-      <AboutContent items={resources.items} />
+      <Hero withCTA={false} {...resourcesProp.banner} />
+      <AboutContent items={resourcesProp.items} />
     </Page>
   );
 }
+
+Resources.propTypes = {
+  resources: PropTypes.shape({}),
+};
+
+Resources.defaultProps = {
+  resources: undefined,
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      resources,
+      title: "Resources",
+    },
+    revalidate: 15 * 60, // 15 minutes
+  };
+}
+
+export default Resources;
