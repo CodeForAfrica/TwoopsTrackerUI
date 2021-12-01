b4f68a72ad8ceee7cb81f2b8c0e69aa4e3824c3b
khadija <khadijamahanga@y7mail.com>
2021-12-01 10:34:13 +0300

    loading svg on storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -6,4 +6,31 @@ module.exports = {
     "storybook-addon-material-ui",
     "storybook-addon-next-router",
   ],
+  webpackFinal: async (config) => {
+    // Need to remove default svg-url-loader first
+    // See: https://github.com/webpack/webpack/issues/595
+    config.module.rules = config.module.rules.map((data) => {
+      const regex = data.test && data.test.toString();
+      if (/svg\|/.test(regex)) {
+        data.test =
+          /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
+      }
+
+      return data;
+    });
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: [
+        "@svgr/webpack",
+        {
+          loader: "svg-url-loader",
+          options: {
+            // https://github.com/bhovhannes/svg-url-loader#iesafe
+            iesafe: true,
+          },
+        },
+      ],
+    });
+    return config;
+  },
 };
