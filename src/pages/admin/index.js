12e30fbd260e6d65eb6ecdc24d7e4681f616f413
_ Kilemensi <clemence@codeforafrica.org>
2021-12-02 17:40:00 +0300

    Switch to pages/admin from public/admin

---
+++ src/pages/admin/index.js
@@ -0,0 +1,41 @@
+import { NextSeo } from "next-seo";
+import Head from "next/head";
+import Script from "next/script";
+import PropTypes from "prop-types";
+import React from "react";
+
+function AdminPage(props) {
+  return (
+    <>
+      <Head>
+        {/* Note the "type" and "rel" attribute values, which are required. */}
+        <link href="/admin/config.yml" type="text/yaml" rel="cms-config-url" />
+      </Head>
+      <NextSeo {...props} />
+      <Script
+        src="https://unpkg.com/netlify-cms@^2.0.0/dist/netlify-cms.js"
+        strategy="beforeInteractive"
+      />
+    </>
+  );
+}
+
+AdminPage.propTypes = {
+  description: PropTypes.string,
+  title: PropTypes.string,
+};
+
+AdminPage.defaultProps = {
+  description: undefined,
+  title: undefined,
+};
+
+export async function getStaticProps() {
+  return {
+    props: {
+      title: "Dashboard",
+    },
+  };
+}
+
+export default AdminPage;
