1aced20df936a84bb6f78164bc46ff8fd4a832bc
_ Kilemensi <clemence@codeforafrica.org>
2021-12-05 21:34:14 +0300

    Switch /investigations to CMS content

--- src/pages/investigations.js
+++ src/pages/investigations.js
@@ -4,44 +4,51 @@ import React from "react";
 import Hero from "@/twoopstracker/components/Hero";
 import InvestigationList from "@/twoopstracker/components/InvestigationList";
 import Page from "@/twoopstracker/components/Page";
-import { investigation } from "@/twoopstracker/config";
+import * as md from "@/twoopstracker/lib/md";
+import { settings } from "@/twoopstracker/lib/settings";
+import content from "content/pages/investigations.md";
 
-export default function Investigations({
-  investigation: investigationProp,
-  ...props
-}) {
+export default function Investigations({ investigations, ...props }) {
+  const { description, title } = props;
+
   return (
     <Page {...props}>
-      <Hero withCTA={false} {...investigationProp.banner} />
-      <InvestigationList items={investigationProp.items} />
+      <Hero description={description} title={title} withCTA={false} />
+      <InvestigationList items={investigations} />
     </Page>
   );
 }
 
 Investigations.propTypes = {
-  investigation: PropTypes.shape({}),
+  description: PropTypes.string,
+  investigations: PropTypes.arrayOf(PropTypes.shape({})),
+  title: PropTypes.string,
 };
 
 Investigations.defaultProps = {
-  investigation: undefined,
-};
-
-Investigations.propTypes = {
-  fallback: PropTypes.arrayOf(PropTypes.shape({})),
-  tweets: PropTypes.arrayOf(PropTypes.shape({})),
-};
-
-Investigations.defaultProps = {
-  fallback: undefined,
-  tweets: undefined,
+  description: undefined,
+  investigations: undefined,
+  title: undefined,
 };
 
 export async function getStaticProps() {
+  const { attributes } = content;
+  attributes.investigations =
+    attributes.investigations
+      ?.map(({ cta, name, thumbnail, url, ...others }) => ({
+        ctaText: cta,
+        image: thumbnail,
+        href: url,
+        title: name,
+        ...others,
+      }))
+      ?.map((i) => md.renderObjectValuesInline(i)) ?? null;
+  const siteSettings = settings();
+
   return {
     props: {
-      description: investigation?.banner?.description ?? null,
-      investigation,
-      title: "Investigations",
+      ...siteSettings,
+      ...attributes,
     },
     revalidate: 15 * 60, // 15 minutes
   };
