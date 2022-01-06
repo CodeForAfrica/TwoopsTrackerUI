c9dbe7ab27356943a20549866f95e6a83beb841f
gertie-sheshe <gertrude.nyenyeshi@gmail.com>
2022-01-06 15:16:24 +0300

    mvp login component

--- src/components/LoginForm/index.js
+++ src/components/LoginForm/index.js
@@ -1,61 +1,18 @@
-import { Button, Grid } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
+import {
+  Button,
+  Typography,
+  InputLabel,
+  Input,
+  FormControl,
+} from "@material-ui/core";
 import { useSession, signIn } from "next-auth/client";
 import Router from "next/router";
 import PropTypes from "prop-types";
 import React, { useEffect } from "react";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    color: "white",
-    background: "none",
-    height: "100vh",
-    margin: "auto",
-  },
-  loginButton: {
-    marginBottom: "1rem",
-    width: "100%",
-    color: "white",
-    "&:hover": {
-      color: "#000",
-    },
-    backgroundColor: theme.palette.primary.main,
-    fontWeight: 800,
-    fontSize: theme.typography.subtitle2.fontSize,
-    height: "3rem",
-    [theme.breakpoints.up("xl")]: {
-      fontSize: theme.typography.subtitle1.fontSize,
-      height: "3.5rem",
-      paddingLeft: "2rem",
-      paddingRight: "2rem",
-    },
-  },
-  buttonContainer: {
-    paddingTop: "1rem",
-    width: "400px",
-    [theme.breakpoints.between("xs", "xs")]: {
-      width: "95vw",
-    },
-    "& .MuiLink-underlineHover": {
-      "&:hover": {
-        textDecoration: "none",
-      },
-    },
-  },
-  formStyles: {
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  item: {
-    color: "black",
-    height: "100vh",
-    display: "flex",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-}));
+import useStyles from "./useStyles";
+
+import Section from "@/twoopstracker/components/Section";
 
 function Login({ providers, ...props }) {
   const classes = useStyles(props);
@@ -68,40 +25,51 @@ function Login({ providers, ...props }) {
   }, [session]);
 
   return (
-    <Grid
-      container
-      justify="space-around"
-      alignItems="center"
-      className={classes.root}
-    >
-      <Grid item xs={12} className={classes.item}>
-        <form noValidate className={classes.formStyles}>
-          <div className={classes.buttonContainer}>
-            {!session &&
-              providers &&
-              Object.values(providers).map((provider) => (
-                <Button
-                  key={provider.name}
-                  value="Subscribe"
-                  name="submit"
-                  id="mc-embedded-subscribe-form"
-                  variant="contained"
-                  className={classes.loginButton}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  onClick={() =>
-                    signIn(provider.id, {
-                      callbackUrl: `${window.location.origin}/explore`,
-                    })
-                  }
-                >
-                  Sign in with {provider.name}
-                </Button>
-              ))}
-          </div>
-        </form>
-      </Grid>
-    </Grid>
+    <Section className={classes.section}>
+      <div>
+        <Typography variant="h2">Welcome back</Typography>
+        <Typography className={classes.text}>
+          Login to access all the features on TrollTracker
+        </Typography>
+        <FormControl>
+          <InputLabel htmlFor="email">Email address</InputLabel>
+          <Input id="email" />
+          <InputLabel htmlFor="password">Password</InputLabel>
+          <Input id="password" />
+        </FormControl>
+        <div>
+          <Button>Edit</Button>
+          <Button>Delete</Button>
+        </div>
+        <div className={classes.buttonContainer}>
+          {!session &&
+            providers &&
+            Object.values(providers).map((provider) => (
+              <Button
+                key={provider.name}
+                value="Subscribe"
+                name="submit"
+                id="mc-embedded-subscribe-form"
+                variant="contained"
+                className={classes.loginButton}
+                target="_blank"
+                rel="noopener noreferrer"
+                onClick={() =>
+                  signIn(provider.id, {
+                    callbackUrl: `${window.location.origin}/explore`,
+                  })
+                }
+              >
+                Sign in with {provider.name}
+              </Button>
+            ))}
+        </div>
+        <Typography className={classes.text}>Forgot password?</Typography>
+        <Typography className={classes.text}>
+          New to TrollTracker? Sign Up now
+        </Typography>
+      </div>
+    </Section>
   );
 }
 
---
+++ src/components/LoginForm/useStyles.js
@@ -0,0 +1,62 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(({ palette, typography, breakpoints }) => ({
+  root: {
+    flexGrow: 1,
+    color: "white",
+    background: "none",
+    height: "100vh",
+    margin: "auto",
+  },
+  section: {
+    marginTop: typography.pxToRem(80),
+    marginBottom: typography.pxToRem(80),
+  },
+  loginButton: {
+    marginBottom: "1rem",
+    width: "100%",
+    color: "white",
+    "&:hover": {
+      color: "#000",
+    },
+    backgroundColor: palette.primary.main,
+    fontWeight: 800,
+    fontSize: typography.subtitle2.fontSize,
+    height: "3rem",
+    [breakpoints.up("xl")]: {
+      fontSize: typography.subtitle1.fontSize,
+      height: "3.5rem",
+      paddingLeft: "2rem",
+      paddingRight: "2rem",
+    },
+  },
+  buttonContainer: {
+    paddingTop: "1rem",
+    width: "400px",
+    [breakpoints.between("xs", "xs")]: {
+      width: "95vw",
+    },
+    "& .MuiLink-underlineHover": {
+      "&:hover": {
+        textDecoration: "none",
+      },
+    },
+  },
+  formStyles: {
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  item: {
+    color: "black",
+    height: "100vh",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    marginTop: typography.pxToRem(22),
+  },
+}));
+
+export default useStyles;
