922199c7ab5990d7999384f794569dc5fd96a2db
Brenda Nyokabi Bucha <brndnyokabi@gmail.com>
2022-01-10 10:20:12 +0300

    Initial commit

---
+++ src/components/TweetSort/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TweetSort = () => {
+  return <div>this is an example tweet sort</div>;
+};
+
+export default TweetSort;
---
+++ src/components/TweetSort/index.stories.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import TweetSort from ".";
+
+export default {
+  title: "Components/TweetSort",
+  argTypes: {},
+};
+
+const Template = () => <TweetSort />;
+
+export const Default = Template.bind({});
+
+Default.args = {};
