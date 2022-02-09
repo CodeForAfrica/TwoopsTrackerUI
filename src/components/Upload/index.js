3fecfec931dcbaa54d97ef2d04f94eac41c58c53
_ Kilemensi <clemence@codeforafrica.org>
2022-02-09 09:00:12 +0300

    Use the default unoptimized Image component

--- src/components/Upload/index.js
+++ src/components/Upload/index.js
@@ -1,7 +1,6 @@
 import { A } from "@commons-ui/core";
 import { Button, makeStyles, Typography } from "@material-ui/core";
 import { getSession } from "next-auth/react";
-import Image from "next/image";
 import PropTypes from "prop-types";
 import React, { useEffect, useCallback } from "react";
 import { useDropzone } from "react-dropzone";
@@ -9,6 +8,7 @@ import { useDropzone } from "react-dropzone";
 import Progress from "./Progress";
 
 import icUpload from "@/twoopstracker/assets/icons/upload.svg";
+import Image from "@/twoopstracker/components/Image";
 import fetchJson from "@/twoopstracker/utils/fetchJson";
 
 const handleUpload = async (content) => {
